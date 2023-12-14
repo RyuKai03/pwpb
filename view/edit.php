1f39c27f4924c79ef9a1054f47daaf7ca34272ee
RyuKai03 <153162405+RyuKai03@users.noreply.github.com>
2023-12-14 10:11:36 +0800

    Update edit.php

--- view/edit.php
+++ view/edit.php
@@ -1,42 +1,42 @@
 <style>
-.parsley-errors-list {
-    list-style-type: none;
-    padding-left: 0;
-    /*padding-bottom: 5px;
-    margin-top: -15px;
-    margin-bottom: 0;*/
-    font-weight: 400;
-    font-size: 11px;
-}
-
-.parsley-errors-list.filled {
-    color: #D43F3A;
-    opacity: 1;
-}
-</style>
-<?
-public function edit($id_buku = null)
-{
-    if (!isset($id_buku)) redirect('daftar');
-
-    $daftar = $this->daftar_model;
-    $validation = $this->form_validation;
-    $validation->set_rules($daftar->rules());
-
-    $data["table_buku"] = $daftar->getById($id_buku);
-
-    if (!$data["table_buku"]) {
-        show_404();
+    .parsley-errors-list {
+        list-style-type: none;
+        padding-left: 0;
+        font-weight: 400;
+        font-size: 11px;
     }
 
-    // ... sisa kode ...
-    // Pastikan variabel $data telah terisi dengan data yang diperlukan untuk halaman edit
-    $this->load->view("header");
-    $this->load->view("sidebar");
-    $this->load->view("edit", $data);
-    $this->load->view("footer");
-}
-?>
+    .parsley-errors-list.filled {
+        color: #D43F3A;
+        opacity: 1;
+    }
+
+    /* Styling notifikasi */
+    .kode-alert {
+        position: relative;
+        padding: 15px;
+        margin: 10px 0;
+        border: 1px solid transparent;
+        border-radius: 4px;
+    }
+
+    .kode-alert.alert1 {
+        background-color: ##00a2ff;
+        border-color: white;
+        color: #ffffff
+    }
+
+    .closed {
+        position: absolute;
+        top: 10px;
+        right: 10px;
+        color: #a94442;
+        text-decoration: none;
+    }
+</style>
+</style>
+
+
 <div class="content">
 
     <!-- Start Page Header -->
@@ -51,14 +51,17 @@ public function edit($id_buku = null)
         <!-- Start Page Header Right Div -->
         <div class="right">
             <div class="btn-group" role="group" aria-label="...">
-                <a href="<?php echo site_url('daftar/') ?>" class="btn btn-light"><i class="fa fa-times"></i>Batal</a>
+                <a href="<?php echo site_url('daftar/') ?>" class="btn btn-light"><i class="fa fa-times"></i> Batal</a>
             </div>
         </div>
         <!-- End Page Header Right Div -->
 
     </div>
     <!-- End Page Header -->
-    <div class="container-default">
+
+ <!-- //////////////////////////////////////////////////////////////////////////// -->
+<!-- START CONTAINER -->
+<div class="container-default">
 
 
 <div class="row">
@@ -67,62 +70,80 @@ public function edit($id_buku = null)
         <div class="col-md-6">
             <div class="panel panel-default">
 
-                <div class="panel-title">
-                    <?php if ($this->session->flashdata('success') && isset($_POST['submit'])): ?>
-                    <div class="kode-alert alert1">
-                        <a href="#" class="closed">&times;</a>
-                       <?php echo $this->session->flashdata('success'); ?>
-                    </div>
-                    <?php endif; ?>
+            <div class="kode-alert alert1" id="notification">
+    <h1 href="#" class="closed" onclick="closeNotification()">&times;</h1>
+    Notifikasi berhasil disimpan.
+</div>
 
-                </div>
+ <script type="text/javascript">
+        $(document).ready(function () {
+            $('.closed').on('click', function () {
+                $(this).closest('.kode-alert').fadeOut();
+            });
 
-         <div class="panel-body">
-                            <form action="" method="post" enctype="multipart/form-data">
+            // Menyembunyikan notifikasi setelah beberapa detik
+            setTimeout(function () {
+                $('.kode-alert').fadeOut();
+            }, 5000); // Misalnya, menyembunyikan notifikasi setelah 5 detik
+        });
+    </script>
+
+
+                        <div class="panel-body">
+                            <form action="" id="editbuku" method="post" enctype="multipart/form-data">
                                 <input type="hidden" name="id_buku" value="<?php echo $table_buku->id_buku ?>" />
-
+                                
                                 <div class="form-group">
                                     <label for="example3" class="form-label">Judul Buku</label>
                                     <input value="<?php echo $table_buku->judul_buku ?>" 
-                                     class="form-control form-control-line <?php echo form_error('judul_buku') ? 'is-invalid':'' ?>" type="text" name="judul_buku" placeholder="judul_buku" required/>
+                                    class ="form-control form-control-line <?php echo form_error('judul_buku') ? 'is-invalid':'' ?>"
+                                    type="text" name="judul_buku" placeholder="judul buku" />
                                     <div class="invalid-feedback">
                                         <?php echo form_error('judul_buku') ?>
                                     </div>
                                 </div>
                                 <div class="form-group">
-                                    <label for="example3" class="form-label">Penerbit Buku</label>
-                                    <input value="<?php echo $table_buku->penerbit_buku ?>" 
-                                     class="form-control form-control-line <?php echo form_error('penerbit_buku') ? 'is-invalid':'' ?>" type="text" name="penerbit_buku" placeholder="penerbit_buku" required/>
-                                    <div class="invalid-feedback">
+                                    <label for="example5"  class="form-label">Penerbit Buku</label>
+                                     <input value="<?php echo $table_buku->penerbit_buku ?>"
+                                     class="form-control form-control-line <?php echo form_error('penerbit_buku') ? 'is-invalid':'' ?>" 
+                                     type="text" name="penerbit_buku" placeholder="penerbit buku" />
+                                    <div class="invalid-feedback has-error">
                                         <?php echo form_error('penerbit_buku') ?>
                                     </div>
                                 </div>
                                 <div class="form-group">
-                                    <label for="example3" class="form-label">Kategori Buku</label>
-                                    <input value="<?php echo $table_buku->kategori_buku ?>" 
-                                     class="form-control form-control-line <?php echo form_error('kategori_buku') ? 'is-invalid':'' ?>" type="text" name="kategori_buku" placeholder="kategori_buku" required/>
-                                    <div class="invalid-feedback">
+                                    <label for="example4" class="form-label">Kategori Buku</label>
+                                    <input value="<?php echo $table_buku->kategori_buku ?>"
+                                     class="form-control form-control-line <?php echo form_error('kategori_buku') ? 'is-invalid':'' ?>" 
+                                      type="text" name="kategori_buku" placeholder="kategori buku" />
+                                    <div class="invalid-feedback has-error">
                                         <?php echo form_error('kategori_buku') ?>
                                     </div>
                                 </div>
                                   <div class="form-group">
-                                    <label for="example3" class="form-label">Tanggal Terbit</label>
-                                    <input value="<?php echo $table_buku->tanggal_terbit ?>" 
-                                     class="form-control form-control-line <?php echo form_error('tanggal_terbit') ? 'is-invalid':'' ?>" type="date" name="tanggal_terbit" placeholder="tanggal_terbit" required/>
-                                    <div class="invalid-feedback">
+                                    <label for="example4" class="form-label">Tanggal Terbit</label>
+                                    <input value="<?php echo $table_buku->tanggal_terbit ?>"
+                                     class="form-control form-control-line <?php echo form_error('tanggal_terbit') ? 'is-invalid':'' ?>" 
+                                     type="date" name="tanggal_terbit" placeholder="tanggal terbit" />
+                                    <div class="invalid-feedback has-error">
                                         <?php echo form_error('tanggal_terbit') ?>
                                     </div>
                                 </div>
+                                <div class="form-group">
+                                    <?php if(!empty($table_buku->cover_buku)) : ?>
+                                        <img src="<?php echo base_url('uploads/'. $table_buku->cover_buku) ?>" alt="" width="128px">
+                                    <?php else : ?>
+                                        <span>Cover tidak tersedia</span>
+                                    <?php endif; ?>
+                                    <input type="hidden" class="form-control" name="cover_buku" value="<?php echo $table_buku->cover_buku; ?>">
+                                    <div class="invalid-feedback has-error">
+                                    </div>
                                 </div>
                                 <div class="form-group">
-                                    <label class="form-label">Cover Buku</label><br>
-                                    <img src="<?php echo base_url('uploads/' . $table_buku->cover_buku) ?>" width="128" style="border: 3px solid black;" />
-                                    <input class="form-control form-control-line" type="hidden" name="cover_buku" 
-                                    value="<?php echo $table_buku->cover_buku ?>" />
-
-                                    <input value="<?php echo $table_buku->cover_buku ?>" class="form-control form-control-line" type="file" name="cover_buku">
+                                    <input type="file" class="form-control form-control-line" name="cover_baru" value="<?php echo $table_buku->cover_buku?>">
                                 </div>
-                                <button type="submit" class="btn btn-default" name="submit">Simpan</button>
+                                </div>
+                                <button type="submit" class="btn btn-default">Simpan</button>
                             </form>
 
                         </div>
@@ -132,7 +153,6 @@ public function edit($id_buku = null)
 
     </div>
 </div>
-
 <!-- END CONTAINER -->
  <!-- //////////////////////////////////////////////////////////////////////////// -->
 
